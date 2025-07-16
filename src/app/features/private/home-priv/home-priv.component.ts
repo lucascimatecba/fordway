da9365d3694ea91473a97ccbf152d4df3d321116
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-16 12:03:40 -0300

    home

--- src/app/features/private/home-priv/home-priv.component.ts
+++ src/app/features/private/home-priv/home-priv.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-priv',
+  standalone: true,
   imports: [],
   templateUrl: './home-priv.component.html',
   styleUrl: './home-priv.component.css'
