4cab1544237f460a786cc23f91749c2b3a6b846a
Lucas Mendes <lms252006@gmail.com>
2025-07-17 19:44:48 -0300

    cards âncora veículos

--- src/app/features/public/home-pub/home-pub.component.ts
+++ src/app/features/public/home-pub/home-pub.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-pub',
@@ -10,10 +10,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home-pub.component.css'
 })
 export class HomePubComponent {
+  constructor(private router: Router) { }
+
   destaques = [
-    { nome: 'Ranger', img: 'assets/ranger_2.jpg' },
-    { nome: 'Mustang', img: 'assets/mustang_2.jpg' },
-    { nome: 'Territory', img: 'assets/territory_2.jpg' },
-    { nome: 'Bronco Sport', img: 'assets/broncoSport_2.jpg' }
+    { id: 1, nome: 'Ranger', img: 'assets/ranger_2.jpg', url: 'https://www.ford.com.br/picapes/ranger/' },
+    { id: 2, nome: 'Mustang', img: 'assets/mustang_2.jpg', url: 'https://www.ford.com.br/performance/mustang/' },
+    { id: 3, nome: 'Territory', img: 'assets/territory_2.jpg', url: 'https://www.ford.com.br/suvs-e-crossovers/territory/' },
+    { id: 4, nome: 'Bronco Sport', img: 'assets/broncoSport_2.jpg', url: 'https://www.ford.com.br/suvs-e-crossovers/bronco-sport/' }
   ];
+
+  irParaDetalhe(id: number) {
+  this.router.navigate(['/veiculo', id]);
+}
 }
