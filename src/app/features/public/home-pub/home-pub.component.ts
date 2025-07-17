191756dbdcb7c1e34a43f36dc0d2faa142f3b5ac
Lucas Mendes <lms252006@gmail.com>
2025-07-16 21:14:47 -0300

    página home pública

--- src/app/features/public/home-pub/home-pub.component.ts
+++ src/app/features/public/home-pub/home-pub.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-pub',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './home-pub.component.html',
   styleUrl: './home-pub.component.css'
 })
 export class HomePubComponent {
-
+  destaques = [
+    { nome: 'Ranger', img: 'assets/ranger_2.jpg' },
+    { nome: 'Mustang', img: 'assets/mustang_2.jpg' },
+    { nome: 'Territory', img: 'assets/territory_2.jpg' },
+    { nome: 'Bronco Sport', img: 'assets/broncoSport_2.jpg' }
+  ];
 }
