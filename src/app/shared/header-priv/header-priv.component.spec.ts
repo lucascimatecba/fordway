78f61547c026838e1e40a9125ab3ae76377b94f0
Lucas Mendes <lms252006@gmail.com>
2025-07-16 21:11:57 -0300

    mudança de nome header

--- src/app/shared/header-priv/header-priv.component.spec.ts
+++ src/app/shared/header-priv/header-priv.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { HeaderPrivComponent } from './header-priv.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('HeaderPrivComponent', () => {
+  let component: HeaderPrivComponent;
+  let fixture: ComponentFixture<HeaderPrivComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      imports: [HeaderPrivComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(HeaderPrivComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
