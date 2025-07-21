f32cd136596866330c408d92d1dab15014caf905
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-21 11:48:19 -0300

    caixa cadastro confirmado

---
+++ src/app/shared/feedback-dialog/feedback-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-feedback-dialog',
+  standalone: true,
+  templateUrl: './feedback-dialog.component.html',
+  styleUrl: './feedback-dialog.component.css',
+  imports: [],
+})
+export class FeedbackDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<FeedbackDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string; action?: string }
+  ) {}
+
+  close(): void {
+    this.dialogRef.close(true); // opcional: pode retornar um valor
+  }
+}
