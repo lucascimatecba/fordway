d7016db298c444c1f1977cebded409cb888d92fa
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-21 11:51:11 -0300

    mudança home privada

--- src/app/features/private/home-priv/home-priv.component.ts
+++ src/app/features/private/home-priv/home-priv.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthPrivService } from '../../../core/services/auth-priv.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-priv',
-  standalone: true,
-  imports: [],
   templateUrl: './home-priv.component.html',
-  styleUrl: './home-priv.component.css'
+  styleUrls: ['./home-priv.component.css']
 })
-export class HomePrivComponent {
+export class HomePrivComponent implements OnInit {
+  nomeUsuario: string = '';
 
+  constructor(
+    private authPrivService: AuthPrivService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const usuario = this.authPrivService.getUsuario();
+    this.nomeUsuario = usuario?.nome ?? 'usuário';
+  }
+
+  irParaDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+
+  irParaClientes(): void {
+    this.router.navigate(['/clientes']);
+  }
 }
